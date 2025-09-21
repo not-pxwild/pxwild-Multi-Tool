8cf1265e4cad9272b547625411efe22221197405
pxwild <johnluca3611@gmail.com>
2025-09-21 20:49:54 +0200

    Update pxwild.py

--- pxwild.py
+++ pxwild.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 pxwild. All rights reserved.
+# This software and its source code are proprietary and confidential.
+# Unauthorized copying, modification, or distribution of this software,
+# via any medium, is strictly prohibited without prior written permission.
+
 import os
 import time
 import importlib.util
@@ -205,3 +210,4 @@ def main():
 if __name__ == "__main__":
 
     main()
+
