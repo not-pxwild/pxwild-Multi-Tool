43ab089f27467b93df426ae361719f1ee0f32dd9
pxwild <johnluca3611@gmail.com>
2025-09-21 20:52:48 +0200

    Update hash_identifier.py

--- osint_tools/hash_identifier.py
+++ osint_tools/hash_identifier.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 pxwild. All rights reserved.
+# This software and its source code are proprietary and confidential.
+# Unauthorized copying, modification, or distribution of this software,
+# via any medium, is strictly prohibited without prior written permission.
+
 import hashlib, os, re
 
 LENGTH_MAP = {
@@ -45,3 +50,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
