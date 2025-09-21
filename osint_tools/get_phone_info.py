c499d8343fcb30b8da11e2022be9c128d352e1fe
pxwild <johnluca3611@gmail.com>
2025-09-21 20:52:36 +0200

    Update get_phone_info.py

--- osint_tools/get_phone_info.py
+++ osint_tools/get_phone_info.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 pxwild. All rights reserved.
+# This software and its source code are proprietary and confidential.
+# Unauthorized copying, modification, or distribution of this software,
+# via any medium, is strictly prohibited without prior written permission.
+
 import phonenumbers
 from phonenumbers import geocoder, carrier, timezone
 
@@ -15,3 +20,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
