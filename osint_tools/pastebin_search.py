94a1d9f358f20f3af1edaa74459733aaa9dd9296
pxwild <johnluca3611@gmail.com>
2025-09-21 20:53:30 +0200

    Update pastebin_search.py

--- osint_tools/pastebin_search.py
+++ osint_tools/pastebin_search.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 pxwild. All rights reserved.
+# This software and its source code are proprietary and confidential.
+# Unauthorized copying, modification, or distribution of this software,
+# via any medium, is strictly prohibited without prior written permission.
+
 import requests, urllib.parse
 
 def psbdmp_search(q: str):
@@ -29,3 +34,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
