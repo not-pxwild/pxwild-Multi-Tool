26f524470b12b6b29405f6012095f7b94273a5cf
pxwild <johnluca3611@gmail.com>
2025-09-21 20:50:38 +0200

    Update Roblox_Info.py

--- programms/Roblox_Info.py
+++ programms/Roblox_Info.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 pxwild. All rights reserved.
+# This software and its source code are proprietary and confidential.
+# Unauthorized copying, modification, or distribution of this software,
+# via any medium, is strictly prohibited without prior written permission.
+
 import requests
 import json
 from datetime import datetime
@@ -108,4 +113,5 @@ def main():
     get_roblox_info()
 
 if __name__ == "__main__":
+
     main()
