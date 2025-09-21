a2d732bd582c470a9d66661ca6f9c7bc00aa4a58
pxwild <johnluca3611@gmail.com>
2025-09-21 20:53:12 +0200

    Update mac_lookup.py

--- osint_tools/mac_lookup.py
+++ osint_tools/mac_lookup.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 pxwild. All rights reserved.
+# This software and its source code are proprietary and confidential.
+# Unauthorized copying, modification, or distribution of this software,
+# via any medium, is strictly prohibited without prior written permission.
+
 import re, requests
 
 MAC_RE = re.compile(r"^[0-9A-Fa-f]{2}([:\-]?[0-9A-Fa-f]{2}){5}$")
@@ -27,3 +32,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
