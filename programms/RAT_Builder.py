db28238da6bca377eb682b4236a2f30a0348fa09
pxwild <johnluca3611@gmail.com>
2025-09-21 20:50:29 +0200

    Update RAT_Builder.py

--- programms/RAT_Builder.py
+++ programms/RAT_Builder.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 pxwild. All rights reserved.
+# This software and its source code are proprietary and confidential.
+# Unauthorized copying, modification, or distribution of this software,
+# via any medium, is strictly prohibited without prior written permission.
+
 import sys
 import os
 import socket
@@ -565,4 +570,5 @@ def main():
     sys.exit(app.exec())
 
 if __name__ == "__main__":
+
     main()
