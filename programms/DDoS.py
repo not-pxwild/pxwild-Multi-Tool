2942ff2e61847083dc9cfbaf22aa37da41d3f9c4
pxwild <johnluca3611@gmail.com>
2025-09-21 20:50:11 +0200

    Update DDoS.py

--- programms/DDoS.py
+++ programms/DDoS.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 pxwild. All rights reserved.
+# This software and its source code are proprietary and confidential.
+# Unauthorized copying, modification, or distribution of this software,
+# via any medium, is strictly prohibited without prior written permission.
+
 import socket
 import socks
 import threading
@@ -675,4 +680,5 @@ def main():
     start_url()
 
 if __name__ == '__main__':
+
     main()
