ad711fdcaeaed636b884ff26f0908918e31adca7
pxwild <johnluca3611@gmail.com>
2025-09-21 20:54:49 +0200

    Update username_checker.py

--- osint_tools/username_checker.py
+++ osint_tools/username_checker.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 pxwild. All rights reserved.
+# This software and its source code are proprietary and confidential.
+# Unauthorized copying, modification, or distribution of this software,
+# via any medium, is strictly prohibited without prior written permission.
+
 import requests
 
 SITES = {
@@ -31,3 +36,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
