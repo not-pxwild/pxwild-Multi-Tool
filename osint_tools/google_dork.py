6d3abb3c3094bbc648fa524de4e550e17545085d
pxwild <johnluca3611@gmail.com>
2025-09-21 20:52:42 +0200

    Update google_dork.py

--- osint_tools/google_dork.py
+++ osint_tools/google_dork.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2025 pxwild. All rights reserved.
+# This software and its source code are proprietary and confidential.
+# Unauthorized copying, modification, or distribution of this software,
+# via any medium, is strictly prohibited without prior written permission.
+
 from googlesearch import search
 
 def main():
@@ -10,3 +15,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
